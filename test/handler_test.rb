326f0732c8c6c1dd3301de5d1d69498d7b00e94c
dalf <dalfgan77@gmail.com>
2019-05-19 15:46:20 +0900

    first commit

---
+++ test/handler_test.rb
@@ -0,0 +1,17 @@
+require_relative '../handler.rb'
+
+require 'test/unit'
+
+class TestHandler < Test::Unit::TestCase
+  def test_happy_path()
+    response = hello(event: {"body": "pow pow pow!"}, context: {})
+    assert_equal 200, response[:statusCode]
+    assert_match /Hello/, response[:body]
+  end
+  
+  def test_sad_path()
+    response = hello(event: nil, context: {})
+    assert_equal 400, response[:statusCode]
+    assert_match /please POST/, response[:body]
+  end
+end
